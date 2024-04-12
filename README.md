c8c676938cce5583bb6a6f84a9442845e245a066
Tokito_to <RustedSilver11@proton.me>
2024-04-12 20:45:27 +0530

    arch: manual partitioning
      - manual partitioning useful for dual boot
      - rank mirrorlist with reflector for faster download of packages
      - inline with official archlinux guide
      - inline with shellcheck

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # arch-simple-installer
-An extremely basic Arch Linux installer, configuring Arch, GRUB, NetworkManager, and iwd.
+#### An extremely basic Arch Linux installer, configuring Arch, GRUB, NetworkManager, and iwd.
 
 # Features
 - Consistently reproducible
-- Combined EFI + BIOS install
 - Installs GRUBv2 portable
 - Follows the Arch Wiki verbatim
 - AMD and Intel microcode install
 - NetworkManager + IWD install
-- Patched for installation on portable media
 - Supports SSH out of the box
 - Bare minimum universal installation
 
@@ -18,11 +16,10 @@ An extremely basic Arch Linux installer, configuring Arch, GRUB, NetworkManager,
 - [alis](https://picodotdev.github.io/alis/): Massive configuration file, does far more than the bare minimum.
 - arch-simple-installer: Automatic partitioning, only 5 manual configs, bare minimum install.
 
-# Demonstration
-https://youtu.be/MxbsB1W98Rw
-
 # Usage
 1. Boot Arch Linux live image
 2. Connect to the internet
-3. `curl -L https://git.io/JU4Mk > installer`
-4. `bash installer`
+```console
+$ wget -O installer bit.ly/3VVIPDu
+$ bash installer`
+```
--- install.sh
+++ install.sh
@@ -5,109 +5,147 @@
 set -e
 
 err() {
-	echo -e " \e[91m*\e[39m $@"
+	echo -e " \e[91m*\e[39m $*"
 	exit 1
 }
 
 prompt() {
-	echo -ne " \e[92m*\e[39m $@"
+	echo -ne " \e[92m*\e[39m $*"
 }
 
+# Check internet Connection
+if ! ping -c1 archlinux.org
+then
+	err "Connect to Internet & try again!"
+fi
+
 # Configuration
-lsblk
-prompt "Disk [/dev/sda]: "
-read DISKPATH
-DISKPATH=${DISKPATH:-/dev/sda}
-[[ ! -b "$DISKPATH" ]] && err "Disk does not exist. Exiting."
+lsblk -o NAME,TYPE,SIZE,FSTYPE,MOUNTPOINTS
+
+prompt "Boot [/dev/sda#]: "
+read -r BOOT_EFI
+[[ ! -b "$BOOT_EFI" ]] && err "Partition does not exist. Exiting."
+
+prompt "Root [/dev/sda#]: "
+read -r ROOT
+[[ ! -b "$ROOT" ]] && err "Partition does not exist. Exiting."
+
+# Home Partition setup
+prompt "Seprate Home Partition [y/N]: "
+read -r HOME_REQUIRED
+if [[ "$HOME_REQUIRED" = "y" ]]
+then
+	prompt "Format Home Partition [y/N]: "
+	read -r FORMAT_HOME
+	[[ "$FORMAT_HOME" = "y" ]] && FORMAT_HOME=Yes || FORMAT_HOME=No
+
+	prompt "Home [/dev/sda#]: "
+	read -r HOME_PARTITION
+	[[ ! -b "$HOME_PARTITION" ]] && err "Partition does not exist. Exiting."
+fi
 
 prompt "Filesystem [ext4]: "
-read FILESYSTEM
+read -r FILESYSTEM
 FILESYSTEM=${FILESYSTEM:-ext4}
-! command -v mkfs.$FILESYSTEM &> /dev/null && err "Filesystem type does not exist. Exiting."
+! command -v mkfs."$FILESYSTEM" &> /dev/null && err "Filesystem type does not exist. Exiting."
 
-prompt "Timezone [America/Los_Angeles]: "
-read TIMEZONE
-TIMEZONE=${TIMEZONE:-America/Los_Angeles}
+prompt "Timezone [Asia/Kolkata]: "
+read -r TIMEZONE
+TIMEZONE=${TIMEZONE:-Asia/Kolkata}
 [[ ! -f "/usr/share/zoneinfo/$TIMEZONE" ]] && err "/usr/share/zoneinfo/$TIMEZONE does not exist. Exiting."
 
-prompt "Hostname [localhost]: "
-read HOSTNAME
-HOSTNAME=${HOSTNAME:-localhost}
+prompt "Mirror Country [India]: "
+read -r COUNTRY
+COUNTRY=${COUNTRY:-India}
+
+prompt "Hostname [archlinux]: "
+read -r HOSTNAME
+HOSTNAME=${HOSTNAME:-archlinux}
 
 prompt "SSH [no]: "
-read SSH
+read -r SSH
 SSH=${SSH:-no}
 
 prompt "Password [root]: "
-read -s PASSWORD
+read -rs PASSWORD
 PASSWORD=${PASSWORD:-root}
-
-# Setup partition variables
-BOOT_BIOS="${DISKPATH}1"
-BOOT_EFI="${DISKPATH}2"
-ROOT="${DISKPATH}3"
 
 echo ""
 echo ""
 printf "%-16s\t%-16s\n" "CONFIGURATION" "VALUE"
-printf "%-16s\t%-16s\n" "Disk:" "$DISKPATH"
-printf "%-16s\t%-16s\n" "Root Filesystem:" "$FILESYSTEM"
-printf "%-16s\t%-16s\n" "Boot Partition [BIOS]:" "$BOOT_BIOS"
+printf "%-16s\t%-16s\n" "Root & Home Filesystem:" "$FILESYSTEM"
 printf "%-16s\t%-16s\n" "Boot Partition [EFI]:" "$BOOT_EFI"
 printf "%-16s\t%-16s\n" "Root Partition:" "$ROOT"
+printf "%-16s\t%-16s\n" "Home Partition:" "$HOME_PARTITION"
+printf "%-16s\t%-16s\n" "Format Home Partition:" "$FORMAT_HOME"
 printf "%-16s\t%-16s\n" "Timezone:" "$TIMEZONE"
+printf "%-16s\t%-16s\n" "Mirror Country:" "$COUNTRY"
 printf "%-16s\t%-16s\n" "Hostname:" "$HOSTNAME"
-printf "%-16s\t%-16s\n" "Password:" "`echo \"$PASSWORD\" | sed 's/./*/g'`"
+printf "%-16s\t%-16s\n" "Password:" "$(echo "$PASSWORD" | sed 's/./*/g')"
 printf "%-16s\t%-16s\n" "SSH:" "$SSH"
 echo ""
 prompt "Proceed? [y/N]: "
-read PROCEED
+read -r PROCEED
 [[ "$PROCEED" != "y" ]] && err "User chose not to proceed. Exiting."
 
 # Unmount for safety
-umount "$BOOT_BIOS" 2> /dev/null || true
+if [ "$HOME_REQUIRED" = "Yes" ]
+then
+	umount "$HOME_PARTITION" 2> /dev/null || true
+fi
 umount "$BOOT_EFI" 2> /dev/null || true
 umount "$ROOT" 2> /dev/null || true
+ 
 
 # Timezone
 timedatectl set-ntp true
 
-# Partitioning
-(
-	echo g		# Erase as GPT
-
-	echo n		# BIOS partition
-	echo
-	echo
-	echo +1M
-	echo t
-	echo 4
-
-	echo n		# EFI partition
-	echo
-	echo
-	echo +512M
-	echo t
-	echo
-	echo 1
-
-	echo n		# Linux root
-	echo
-	echo
-	echo
-	sleep 3		# Delay to avoid race condition
-	echo w		# Write
-) | fdisk -w always -W always "$DISKPATH"
-
 # Formatting partitions
-mkfs.fat -F 32 "$BOOT_EFI"
-yes | mkfs.$FILESYSTEM "$ROOT"
+#mkfs.fat -F 32 "$BOOT_EFI"
+if [[ $(blkid "$BOOT_EFI" | grep -o 'TYPE="[^"]*"' | awk -F '"' '{print $2}') != "vfat" ]]
+then
+	err "Unsupported Boot Partition: $BOOT_EFI. Exiting"
+fi
 
-# Mount our new partition
+echo "Formating Root Partition: $ROOT"
+yes | mkfs."$FILESYSTEM" "$ROOT"
+
+if [ "$FORMAT_HOME" = "Yes" ]
+then
+	echo "Formating Home Partition: $HOME_PARTITION"
+	yes | mkfs."$FILESYSTEM" "$HOME_PARTITION"
+fi
+
+# Mount our new partition #Delay to avoid race condition
+echo "Mounting Root: $ROOT..."
 mount "$ROOT" /mnt
+sleep 3
+
+if [ "$HOME_REQUIRED" = "y" ]
+then
+	echo "Mounting Home: $HOME_PARTITION..."
+  mount --mkdir "$HOME_PARTITION" /mnt/home
+	sleep 3
+fi
+
+echo "Mounting Boot: $BOOT_EFI..."
+mount --mkdir "$BOOT_EFI" /mnt/boot
+sleep 3
+
+# Update Mirrors
+pacman -Syy #Force Update Current Package Repo
+echo "Backup Current Mirrorlist"
+cp -v /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
+echo "Updating Mirrors"
+reflector --save /etc/pacman.d/mirrorlist --country "$COUNTRY" --sort rate --latest 20 --verbose
+
+# Enable Parallel downloading
+sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 4/" /etc/pacman.conf
+sed -i "/Color/"'s/^#//' /etc/pacman.conf
 
 # Initialize base system, kernel, and firmware
-pacstrap /mnt base linux linux-firmware
+pacstrap -K /mnt base linux linux-firmware
 
 # Setup fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -136,27 +174,18 @@ genfstab -U /mnt >> /mnt/etc/fstab
 	# Install GRUBv2 as a removable drive (universal across hw)
 	echo "pacman -Sy --noconfirm grub efibootmgr"
 
-	# BIOS steps
-	echo "grub-install --target=i386-pc \"$DISKPATH\" --recheck"
-
-	# EFI steps
-	echo "mkdir /boot/efi"
-	echo "mount \"$BOOT_EFI\" /boot/efi"
-	echo "grub-install --target=x86_64-efi --efi-directory=/boot/efi --removable --recheck"
-	
-	# Install GRUB config
+	# Install GRUBv2
+	echo "pacman -Sy --noconfirm --needed grub efibootmgr"
+	echo "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=\"ArchLinux\" --recheck"
 	echo "grub-mkconfig -o /boot/grub/grub.cfg"
 
 	# Install and enable NetworkManager on boot
 	echo "pacman -Sy --noconfirm networkmanager iwd"
 	echo "systemctl enable NetworkManager"
 	
-	# Launch bluetoothd on boot
+	# Launch Bluetoothd on boot
+	echo "pacman -Sy --noconfirm --needed bluez"
 	echo "systemctl enable bluetooth"
-
-	# Fix initramfs for portable media
-	echo "sed -i \"s/autodetect modconf block filesystems keyboard/block keyboard autodetect modconf filesystems/\" /etc/mkinitcpio.conf"
-	echo "mkinitcpio -P"
 
 	# Enable SSH server out of the box
 	if [[ "$SSH" == "yes" ]]
@@ -167,4 +196,4 @@ genfstab -U /mnt >> /mnt/etc/fstab
 	fi
 ) | arch-chroot /mnt
 
-echo "Install completed on $DISKPATH." 
+echo "Basic ArchLinux Install completed."
